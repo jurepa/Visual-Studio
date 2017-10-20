ccb0373d893bb4bde9041c7919dc19c966cb7847
PJarana <pablojosejarana21@gmail.com>
2017-10-20 12:11:41 +0200

    ListPersona

---
+++ MVC-ListPersona/MVC-ListPersona/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using MVC_ListPersona.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_ListPersona.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ListadoPersonas()
+        {
+            ListPersona listado = new ListPersona();
+            return View(listado);
+        }
+    }
+}
