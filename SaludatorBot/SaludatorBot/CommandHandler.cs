5b6eae88b93044ff7662597b6d7d8d2758e34aed
PJarana <pablojosejarana21@gmail.com>
2017-10-24 22:51:35 +0200

    Intento de bot en discord

---
+++ SaludatorBot/SaludatorBot/CommandHandler.cs
@@ -0,0 +1,41 @@
+﻿using Discord.Commands;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaludatorBot
+{
+    public class CommandHandler
+    {
+        private DiscordSocketClient client;
+        private CommandService service;
+        public CommandHandler(DiscordSocketClient client)
+        {
+            this.client = client;
+            service = new CommandService();
+            service.AddModulesAsync(Assembly.GetEntryAssembly());
+            client.MessageReceived +=HandleCommandAsync;
+        }
+
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            int argPos = 0;
+            SocketUserMessage msg = (SocketUserMessage)s;
+            SocketCommandContext context = new SocketCommandContext(client,msg);
+            if (msg.HasCharPrefix('!', ref argPos))
+            {
+               var result= await service.ExecuteAsync(context, argPos);
+
+                if (!result.IsSuccess && result.Error!=CommandError.UnknownCommand)
+                {
+                    await context.Channel.SendMessageAsync(result.ErrorReason);
+                }
+            }
+        }
+    }
+}
---
+++ SaludatorBot/SaludatorBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaludatorBot
+{
+    class Program
+
+    {
+        private DiscordSocketClient client;
+        private CommandHandler handler;
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+        public async Task StartAsync()
+        {
+            client = new DiscordSocketClient();
+            new CommandHandler(client);
+            await client.LoginAsync(Discord.TokenType.Bot, "MzcyNDI2NzUyMzM4ODIxMTIw.DNEA9A.0pAr324Jlo-G0v3D0nfSLrpas0Q");
+            await client.StartAsync();
+            handler = new CommandHandler(client);
+            await Task.Delay(-1);
+            
+        }
+    }
+
+
+}
