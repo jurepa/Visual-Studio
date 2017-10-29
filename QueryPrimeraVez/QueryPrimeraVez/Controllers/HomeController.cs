11226f03da6a050f92d965a2c0d0196542c5300e
PJarana <pablojosejarana21@gmail.com>
2017-10-29 13:10:57 +0100

    Ej QueryPrimeraVez
    Completado con ViewData

---
+++ QueryPrimeraVez/QueryPrimeraVez/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QueryPrimeraVez.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(bool? miPrimeraVez)
+        {
+            if (miPrimeraVez == false)
+            {
+                ViewData["primeraVez"] = "Ya no es tu primera vez";
+                ViewData["esMiPrimeraVez"] = miPrimeraVez;
+            }
+            else
+            {
+                ViewData["primeraVez"] = "Es tu primera vez";
+                miPrimeraVez = false;
+                ViewData["esMiPrimeraVez"] = miPrimeraVez;
+            }
+            return View();
+        }
+    }
+}
