75fe3532fb872575f27f9d7fdf6a672d7ba84c80
PJarana <pablojosejarana21@gmail.com>
2017-11-06 14:28:20 +0100

    Aprendiendo Binding

---
+++ BindingSliderFontSize/BindingSliderFontSize/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace BindingSliderFontSize
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+
+
+        private void TextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+
+            if (System.Text.RegularExpressions.Regex.IsMatch(e.Key.ToString(), "[0-9]"))
+            {
+                e.Handled = false;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+
+            
+        }
+
+       /* private void textNumbers_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            if(!String.IsNullOrWhiteSpace(textNumbers.Text.ToString()))
+            {
+                textBlock.FontSize = int.Parse(sender.Text.ToString());
+            }
+            
+        }*/
+    }
+}
