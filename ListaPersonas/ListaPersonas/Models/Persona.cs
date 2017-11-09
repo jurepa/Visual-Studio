7f1a57f33fce24222a534f0ea8da5837b2e62bde
PJarana <pablojosejarana21@gmail.com>
2017-11-09 12:51:24 +0100

    up

---
+++ ListaPersonas/ListaPersonas/Models/Persona.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+
+namespace ListaPersonas.Models
+{
+    /// <summary>
+    /// Clase Persona, con idDepartamento
+    /// </summary>
+    public class Persona
+    {
+        
+        public int idPersona { get; set; }
+
+        public string nombre { get; set; }
+        public string apellidos { get; set; }
+
+        public DateTime fechaNac { get; set; }
+        
+        public string direccion { get; set; }
+
+        public string telefono { get; set; }
+        public int idDepartamento { get; set; }
+    }
+
+}
---
+++ ListaPersonas/ListaPersonas/ViewModel/ListPersonaConPersonaSeleccionada.cs
@@ -0,0 +1,58 @@
+﻿using ListaPersonas.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPersonas.ViewModel
+{
+    public class ListPersonaConPersonaSeleccionada :INotifyPropertyChanged
+    {
+        #region "Atributos"
+        private ObservableCollection<Persona> _listado;
+        private Persona _personaSeleccionada;
+        #endregion
+        public event PropertyChangedEventHandler PropertyChanged;
+        #region "Constructores"
+        public ListPersonaConPersonaSeleccionada()
+        {
+            ListPersona lista = new ListPersona();
+            this._listado = lista.listado;
+        }
+        #endregion
+        
+        #region "Propiedades públicas"
+        public ObservableCollection<Persona> listado
+        {
+            get { return _listado; }
+        }
+        public Persona personaSeleccionada
+        {
+            get { return _personaSeleccionada; }
+            set
+            {
+                _personaSeleccionada = value;
+                //Notificación de cambio a la vista
+                NotifyPropertyChanged("personaSeleccionada");
+            }
+        }
+        #endregion
+        /// <summary>
+        /// Método para lanzar el evento
+        /// </summary>
+        /// <param name="propertyName"></param>
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+
+
+    }
+}
