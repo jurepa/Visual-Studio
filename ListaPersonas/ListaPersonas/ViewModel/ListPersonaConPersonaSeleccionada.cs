9071e8a60d1bc8cdb1e75cd53c8d092777e260a9
PJarana <pablojosejarana21@gmail.com>
2017-11-16 13:03:35 +0100

    up

--- ListaPersonas/ListaPersonas/ViewModel/ListPersonaConPersonaSeleccionada.cs
+++ ListaPersonas/ListaPersonas/ViewModel/ListPersonaConPersonaSeleccionada.cs
@@ -1,4 +1,5 @@
-﻿using ListaPersonas.Models;
+﻿using _13_DataContext.ViewModels;
+using ListaPersonas.Models;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -9,13 +10,12 @@ using System.Threading.Tasks;
 
 namespace ListaPersonas.ViewModel
 {
-    public class ListPersonaConPersonaSeleccionada :INotifyPropertyChanged
+    public class ListPersonaConPersonaSeleccionada :clsVMBase
     {
         #region "Atributos"
         private ObservableCollection<Persona> _listado;
         private Persona _personaSeleccionada;
         #endregion
-        public event PropertyChangedEventHandler PropertyChanged;
         #region "Constructores"
         public ListPersonaConPersonaSeleccionada()
         {
@@ -44,13 +44,7 @@ namespace ListaPersonas.ViewModel
         /// Método para lanzar el evento
         /// </summary>
         /// <param name="propertyName"></param>
-        private void NotifyPropertyChanged(string propertyName)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
-        }
+
 
 
 
