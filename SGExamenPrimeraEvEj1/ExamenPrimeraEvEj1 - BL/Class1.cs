5075b12fbb6ee3ae97d2928016ac06b06adde1a4
PJarana <pablojosejarana21@gmail.com>
2017-11-27 21:30:45 +0100

    Plantillas examen a tope

---
+++ SGExamenPrimeraEvEj1/ExamenPrimeraEvEj1 - BL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPrimeraEvEj1___BL
+{
+    public class Class1
+    {
+    }
+}
---
+++ SGExamenPrimeraEvEj2/SGExamenPrimeraEvEj2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SGExamenPrimeraEvEj2.MvcApplication" Language="C#" %>
