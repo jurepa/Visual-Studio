2ce323a6e82b004f66ba1c9d148b39ba11114327
PJarana <pablojosejarana21@gmail.com>
2017-12-01 13:50:26 +0100

    up

---
+++ 17-CRUD-Personas-UWP/17-CRUD-Personas-UWP-DAL/ListadoPersonasDAL.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using _17_CRUD_Personas_UWP_ET;
+
+namespace _17_CRUD_Personas_UWP_DAL
+{
+    public class ListadoPersonasDAL
+    {
+        public List<Persona> listado { get; set; }
+
+        public ListadoPersonasDAL()
+        {
+            this.listado = new List<Persona>();
+        }
+        public List<Persona> getPersonas()
+        {
+            Connection cx = new Connection();
+            SqlDataReader lector;
+            Persona p;
+            SqlCommand consulta = new SqlCommand();
+            try
+            {
+                consulta.CommandText = "Select*From Personas";
+                consulta.Connection = cx.conexion;
+                lector = consulta.ExecuteReader();
+
+                if (lector.HasRows)
+                {
+                    while (lector.Read())
+                    {
+                        p = new Persona();
+                        p.idPersona = (int)lector["ID"];
+                        p.nombre = (string)lector["nombre"];
+                        p.apellidos = (string)lector["apellidos"];
+                        p.direccion = (string)lector["direccion"];
+                        p.telefono = (string)lector["telefono"];
+                        p.idDepartamento = (int)lector["ID_Departamento"];
+                        this.listado.Add(p);
+                    }
+                }
+                cx.closeConnection();
+            }
+            catch (SqlException )
+            {
+                throw;
+            }
+
+            return this.listado;
+        }
+        public Persona getPersona(int id)
+        {
+            Connection cx = new Connection();
+            SqlDataReader lector;
+            Persona p =null;
+            SqlCommand consulta = new SqlCommand();
+            SqlParameter idParam=new SqlParameter();
+            try
+            {
+                idParam.ParameterName = "@id";
+                idParam.SqlDbType = System.Data.SqlDbType.Int;
+                idParam.Value = id;
+                consulta.Parameters.Add(idParam);
+                consulta.CommandText = "Select*From Personas where id=@id";
+                consulta.Connection = cx.conexion;
+                lector = consulta.ExecuteReader();
+
+                if (lector.HasRows)
+                {
+                        lector.Read();
+                        p = new Persona();
+                        p.idPersona = (int)lector["ID"];
+                        p.nombre = (string)lector["nombre"];
+                        p.apellidos = (string)lector["apellidos"];
+                        p.direccion = (string)lector["direccion"];
+                        p.telefono = (string)lector["telefono"];
+                        p.idDepartamento = (int)lector["ID_Departamento"];
+
+                }
+                cx.closeConnection();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return p;
+        }
+
+        public void updatePersona(Persona p)
+        {
+            Connection cx = new Connection();
+            SqlCommand consulta = new SqlCommand();
+            SqlParameter nombre = new SqlParameter() ;
+            SqlParameter apellidos = new SqlParameter();
+            SqlParameter id = new SqlParameter();
+            SqlParameter idDepartamento = new SqlParameter();
+            SqlParameter direccion = new SqlParameter();
+            SqlParameter telefono = new SqlParameter();
+            try
+            {
+                nombre.ParameterName = "@nombre";
+                nombre.SqlDbType = System.Data.SqlDbType.NVarChar;
+                nombre.Value = p.nombre;
+
+                idDepartamento.ParameterName = "@idDepartamento";
+                idDepartamento.SqlDbType = System.Data.SqlDbType.Int;
+                idDepartamento.Value = p.idDepartamento;
+
+                apellidos.ParameterName = "@apellidos";
+                apellidos.SqlDbType = System.Data.SqlDbType.NVarChar;
+                apellidos.Value = p.apellidos;
+
+                direccion.ParameterName = "@direccion";
+                direccion.SqlDbType = System.Data.SqlDbType.NVarChar;
+                direccion.Value = p.direccion;
+
+                telefono.ParameterName = "@telefono";
+                telefono.SqlDbType = System.Data.SqlDbType.Char;
+                telefono.Value = p.telefono;
+
+                id.ParameterName = "@id";
+                id.SqlDbType = System.Data.SqlDbType.Int;
+                id.Value = p.idPersona;
+
+                consulta.Parameters.Add(nombre);
+                consulta.Parameters.Add(apellidos);
+                consulta.Parameters.Add(direccion);
+                consulta.Parameters.Add(telefono);
+                consulta.Parameters.Add(id);
+                consulta.Parameters.Add(idDepartamento);
+                consulta.CommandText = "Update Personas Set Nombre=@nombre, Apellidos=@apellidos, Direccion=@direccion, Telefono=@telefono, ID_Departamento=@idDepartamento WHERE ID=@id";
+                consulta.Connection = cx.conexion;
+                consulta.ExecuteNonQuery();
+                cx.closeConnection();
+            }
+            catch (SqlException e)
+            {
+                throw;
+            }
+        }
+        public void deletePersona(int id)
+        {
+            Connection cx = new Connection();
+            SqlCommand consulta = new SqlCommand();
+
+            SqlParameter idParam = new SqlParameter();
+
+            try
+            {
+
+
+                idParam.ParameterName = "@id";
+                idParam.SqlDbType = System.Data.SqlDbType.Int;
+                idParam.Value = id;
+                consulta.Parameters.Add(idParam);
+                consulta.CommandText = "DELETE FROM PERSONAS WHERE ID=@id ";
+                consulta.Connection = cx.conexion;
+                consulta.ExecuteNonQuery();
+                cx.closeConnection();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+        }
+        public void insertPersona(Persona p)
+        {
+            Connection cx = new Connection();
+            SqlCommand consulta = new SqlCommand();
+            SqlParameter nombre = new SqlParameter();
+            SqlParameter apellidos = new SqlParameter();
+            SqlParameter direccion = new SqlParameter();
+            SqlParameter telefono = new SqlParameter();
+            try
+            {
+                nombre.ParameterName = "@nombre";
+                nombre.SqlDbType = System.Data.SqlDbType.NVarChar;
+                nombre.Value = p.nombre;
+
+                apellidos.ParameterName = "@apellidos";
+                apellidos.SqlDbType = System.Data.SqlDbType.NVarChar;
+                apellidos.Value = p.apellidos;
+
+                direccion.ParameterName = "@direccion";
+                direccion.SqlDbType = System.Data.SqlDbType.NVarChar;
+                direccion.Value = p.direccion;
+
+                telefono.ParameterName = "@telefono";
+                telefono.SqlDbType = System.Data.SqlDbType.Char;
+                telefono.Value = p.telefono;
+
+
+                consulta.Parameters.Add(nombre);
+                consulta.Parameters.Add(apellidos);
+                consulta.Parameters.Add(direccion);
+                consulta.Parameters.Add(telefono);
+                consulta.CommandText = "INSERT INTO PERSONAS (Nombre,Apellidos,Direccion,Telefono) VALUES (@nombre,@apellidos,@direccion,@telefono)";
+                consulta.Connection = cx.conexion;
+                consulta.ExecuteNonQuery();
+                cx.closeConnection();
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ 17-CRUD-Personas-UWP/17-CRUD-Personas-UWP/ViewModel/ListPersonaConPersonaSeleccionada.cs
@@ -0,0 +1,227 @@
+﻿using _17_CRUD_Personas_UWP_BL;
+using _17_CRUD_Personas_UWP_ET;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17_CRUD_Personas_UWP.ViewModel
+{
+    public class ListPersonaConPersonaSeleccionada :clsVMBase
+    {
+        #region "Atributos"
+        private ObservableCollection<Persona> _listado;
+        private ObservableCollection<Persona> _listadoAux;
+        private Persona _personaSeleccionada;
+        private string _textoBusqueda;
+        private DelegateCommand _delete;
+        private DelegateCommand _addPersona;
+        private DelegateCommand _savePersona;
+        //private DelegateCommand _searchPersona;
+        #endregion
+        #region "Constructores"
+        public ListPersonaConPersonaSeleccionada()
+        {
+            ListadoPersonasBL listado = new ListadoPersonasBL();
+            this._listado = new ObservableCollection<Persona>(listado.getListadoBL());
+            this._listadoAux = this._listado;
+        }
+        #endregion
+        
+        #region "Propiedades públicas"
+        public DelegateCommand delete
+        {
+            get
+            {
+                _delete= new DelegateCommand(ExecuteDelete, CanExecuteDelete);
+                return _delete;
+            }
+
+            set
+            {
+                _delete = value;
+            }
+        }
+        public ObservableCollection<Persona> listadoAux
+        {
+            get
+            {
+                return _listadoAux;
+            }
+
+            set
+            {
+                _listadoAux = value;
+                NotifyPropertyChanged("listadoAux");
+            }
+        }
+        public string textoBusqueda
+        {
+            get
+            {
+                return _textoBusqueda;
+            }
+
+            set
+            {
+                _textoBusqueda = value;
+                //_searchPersona.RaiseCanExecuteChanged();
+                if (String.IsNullOrWhiteSpace(_textoBusqueda))
+                {
+                    _listadoAux = _listado;
+                    NotifyPropertyChanged("listadoAux");
+                }
+                else
+                {
+                    buscaPersona();
+                }
+            }
+        }
+        public DelegateCommand addPersona
+        {
+            get
+            {
+                _addPersona = new DelegateCommand(ExecuteAddPersona);
+                return _addPersona;
+            }
+            set
+            {
+                _addPersona = value;
+            }
+        }
+        public DelegateCommand savePersona
+        {
+            get
+            {
+                _savePersona = new DelegateCommand(ExecuteSavePersona);
+                return _savePersona;
+            }
+            set
+            {
+                _savePersona = value;
+            }
+        }
+       /* public DelegateCommand searchPersona
+        {
+            get
+            {
+                _searchPersona = new DelegateCommand(ExecuteSearchPersona,CanExecuteSearchPersona);
+                return _searchPersona;
+                
+            }
+
+            set
+            {
+                _searchPersona = value;
+            }
+        }*/
+        public ObservableCollection<Persona> listado
+        {
+            get { return _listado; }
+        }
+        public Persona personaSeleccionada
+        {
+            get { return _personaSeleccionada; }
+            set
+            {
+                _personaSeleccionada = value;
+                _delete.RaiseCanExecuteChanged();
+                _savePersona.RaiseCanExecuteChanged();
+                //Notificación de cambio a la vista
+                NotifyPropertyChanged("personaSeleccionada");
+            }
+        }
+        #endregion
+        public bool CanExecuteDelete()
+        {
+            bool canExecute = false;
+            if (_personaSeleccionada != null)
+            {
+                canExecute = true;
+            }
+            return canExecute;
+        }
+
+        public void ExecuteSearchPersona()
+        {
+            listadoAux = new ObservableCollection<Persona>();
+            NotifyPropertyChanged("listadoAux");
+            string nombre=null;
+            for (int i = 0; i < _listado.Count; i++)
+            {
+                nombre =_listado.ElementAt(i).nombre.ToLower();
+                if (nombre.Contains(textoBusqueda.ToLower()))
+                {
+                    listadoAux.Add(_listado.ElementAt(i));
+                    NotifyPropertyChanged("listadoAux");
+                }
+            }
+        }
+        public bool CanExecuteSearchPersona()
+        {
+            bool encontrado = false;
+            if (!String.IsNullOrEmpty(textoBusqueda))
+            {
+                encontrado = true;
+            }
+            else
+            {
+                listadoAux = listado;
+                NotifyPropertyChanged("listadoAux");
+            }
+            return encontrado;
+        }
+        public void ExecuteDelete()
+        {
+            listadoAux.Remove(_personaSeleccionada);
+            listado.Remove(_personaSeleccionada); //No borra la persona seleccionada en el listado original, dnt know why
+            NotifyPropertyChanged("listado");
+            NotifyPropertyChanged("listadoAux");
+        }
+        public void ExecuteAddPersona()
+        {
+            _personaSeleccionada = new Persona();
+            NotifyPropertyChanged("personaSeleccionada");
+        }
+        //private bool canExecuteSavePersona()
+        //{
+        //    bool sePuede = false;
+        //    if (_personaSeleccionada != null)
+        //    {
+        //        sePuede = true;
+
+        //    }
+        //    return sePuede;
+        //}
+        private void ExecuteSavePersona()
+        {
+            if (_personaSeleccionada.idPersona == 0)
+            {
+                _personaSeleccionada.idPersona=listadoAux.ElementAt(listado.Count - 1).idPersona+1;
+                NotifyPropertyChanged("personaSeleccionada");
+                listadoAux.Add(_personaSeleccionada);
+                NotifyPropertyChanged("listadoAux");
+            }
+        }
+
+        private void buscaPersona()
+        {
+            _listadoAux = new ObservableCollection<Persona>();
+            NotifyPropertyChanged("listadoAux");
+            string nombre = null;
+            for (int i = 0; i < _listado.Count; i++)
+            {
+                nombre = _listado.ElementAt(i).nombre.ToLower();
+                if (nombre.Contains(_textoBusqueda.ToLower()))
+                {
+                    _listadoAux.Add(_listado.ElementAt(i));
+                    NotifyPropertyChanged("listadoAux");
+                }
+            }
+
+        }
+    }
+}
