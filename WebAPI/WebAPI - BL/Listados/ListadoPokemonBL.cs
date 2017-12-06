2c72a4ff9a2f2cb60d1ce6495ca949f0cacb59c8
PJarana <pablojosejarana21@gmail.com>
2017-12-06 18:41:13 +0100

    Web api de pokemon en camino

---
+++ WebAPI/WebAPI - BL/Listados/ListadoPokemonBL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebApi___DAL.Listados;
+using WebAPI___ET;
+
+namespace WebAPI___BL.Listados
+{
+    public class ListadoPokemonBL
+    {
+        public List<Pokemon> getListadoBL()
+        {
+            ListadoPokemonDAL listadoPokemon = new ListadoPokemonDAL();
+            List<Pokemon> listadoParaUI = listadoPokemon.getPokemons();
+
+            return listadoParaUI;
+        }
+        public Pokemon getPokemon(int id)
+        {
+            ListadoPokemonDAL listadoPokemon = new ListadoPokemonDAL();
+
+            Pokemon PokemonUI = listadoPokemon.getPokemon(id);
+
+            return PokemonUI;
+        }
+
+        public void updatePokemon(Pokemon p)
+        {
+            ListadoPokemonDAL listado = new ListadoPokemonDAL();
+            listado.updatePokemon(p);
+        } 
+        public void deletePokemon(int id)
+        {
+            ListadoPokemonDAL listado = new ListadoPokemonDAL();
+            listado.deletePokemon(id);
+        }
+        public void insertPokemon(Pokemon p)
+        {
+            ListadoPokemonDAL listado = new ListadoPokemonDAL();
+            listado.insertPokemon(p);
+        }
+    }
+}
