70600a474dd7c01eb6f9b9d7898a2a6daceae3f9
PJarana <pablojosejarana21@gmail.com>
2017-12-19 14:35:59 +0100

    up

---
+++ WebJS/javascript/JavaScript.js
@@ -0,0 +1,32 @@
+﻿window.onload = inicializaEventos;
+
+function inicializaEventos()
+{
+    document.getElementById("btnSaludar").addEventListener("click",saludar,false);
+}
+
+function saludar()
+{
+    var nombre = document.getElementById("txtNombre").value;
+    var fechaString = document.getElementById("txtFecha").value;
+    var fechaDate = Date.parse(fechaString);
+    if (nombre === "") 
+    {
+        document.getElementById("labelErrorNombre").innerHTML = "Error";
+    }
+    else
+    {
+        document.getElementById("labelErrorNombre").innerHTML = "";
+    }
+    if (isNaN(fechaDate)) {
+        document.getElementById("labelErrorFecha").innerHTML = "Mala fecha loquete, el formato es mm/dd/yyyy";
+    }
+    else
+    {
+        document.getElementById("labelErrorFecha").innerHTML = "Qué buena fesha io";
+    }
+    if (nombre != "" && !isNaN(fechaDate))
+    {
+        alert("Hola " + nombre + " crack, máquina, fiera, leyenda, maestro, machote, churraca. Naciste el " + new Date(fechaDate).toDateString());
+    }
+}
