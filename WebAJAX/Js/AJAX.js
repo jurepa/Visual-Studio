1bbb246591e052963453148a9e2e68bb3b6bd2d2
PJarana <pablojosejarana21@gmail.com>
2018-01-12 12:55:34 +0100

    up

--- WebAJAX/Js/AJAX.js
+++ WebAJAX/Js/AJAX.js
@@ -2,7 +2,8 @@
 
 function inicializaEventos()
 {
-    document.getElementById("btnHola").addEventListener("click",SaludaCrack,false);
+    document.getElementById("btnHola").addEventListener("click", SaludaCrack, false);
+    document.getElementById("btnListaXML").addEventListener("click", MuestraXML, false);
 }
 
 function SaludaCrack()
@@ -30,7 +31,58 @@ function SaludaCrack()
 
     //PASO 5: Enviar la solicitud
     miPeticion.send();
+}
 
+function MuestraXML()
+{
+    var miPeticion = new XMLHttpRequest();
 
+    miPeticion.open("GET", "../Server/Personas.xml", true);
+    miPeticion.onreadystatechange = function ()
+    {
+        document.getElementById("divPersonasXML").innerHTML = "Cargando.........................";
+        if (miPeticion.readyState == 4 && miPeticion.status == 200)//Si el servidor llega al readyState 4 y el servidor da el codigo OK
+        {
+            var table = document.createElement("table");
+            var tr = document.createElement("tr");
+            var tdNombre = document.createElement("td");
+            var tdApellido = document.createElement("td");
+            tdNombre.innerHTML = "Nombres";
+            tdApellido.innerHTML = "Apellidos";
+            tr.appendChild(tdNombre);
+            tr.appendChild(tdApellido);
+            table.appendChild(tr);
+            //setAttributes border
+            
+            var misDatosXML = miPeticion.responseXML;
+            //document.getElementById("divPersonasXML").innerHTML = misDatosXML.getElementsByTagName("persona")[1].getElementsByTagName("nombre")[0].firstChild.nodeValue;
+            var arrayPersonas = misDatosXML.getElementsByTagName("persona");
+            for (var i = 0; i < arrayPersonas.length; i++)
+            {
+                var persona = arrayPersonas[i];
+                var fila=table.insertRow(i);
+                for (var j = 1; j <= persona.childElementCount; j++)
+                {
+                    var td1 = document.createElement("td");
+                    if (persona.childNodes[j].firstChild.nodeType == 1)
+                    {
+                        j++;
+                        td1.innerHTML = persona.childNodes[j].firstChild.nodeValue;
+                    }
+                    fila.appendChild(td1);
+                }
+                //var td1 = document.createElement("td");
+                //td1.innerHTML = persona.getElementsByTagName("nombre")[0].firstChild.nodeValue;
+                //var td2 = document.createElement("td");
+                //td2.innerHTML = persona.getElementsByTagName("apellidos")[0].firstChild.nodeValue;
+                //fila.appendChild(td1);
+                //fila.appendChild(td2);
+                //table.appendChild(fila);
+            }
+            document.getElementById("divPersonasXML").appendChild(table);
+        }
+        //alert(miPeticion.readyState + " " + miPeticion.status);
+    }
+    miPeticion.send();
 
 }
